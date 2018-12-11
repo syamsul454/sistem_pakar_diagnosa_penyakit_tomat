714b40ce5fdd950ebde6f46eef07f011d3376976
syamsul454 <andibrigjent@gmail.com>
2018-12-11 20:02:43 +0800

    coba

--- application/views/halaman/index.php
+++ application/views/halaman/index.php
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
-
-<form method="post" action="<?= site_url('halaman/'. $url) ?>">
-	<label> <h2> <?= $daun  ?> </h2></label>
-	<button name="pertanyaan" value="daun">ya</button>
-	<button name="pertanyaan" value="buah">tidak</button>
-	lgjhg
-</form>
-=======
 <form method="post" action="<?= site_url($url) ?>">
 	<label><?= $daun  ?></label>
 	<button name="pertanyaan" value="ya">ya</button>
 	<button name="pertanyaan" value="tidak">tidak</button>
 </form>
 	
->>>>>>> 7873f49bff5c3ec3e6ed70bf57d0a12d9ff7f3ff
+
